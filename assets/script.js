a10ea3fa54d131ec8874a0067afb3e2f4de6ea30
Alhassane Seye <seyealhassane@gmail.com>
2023-01-21 15:13:41 +0100

    projet 3

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+fetch("http://localhost:5678/api/works")
+    .then((response) => response.json())
+    .then (data =>{console.log(data);
+        for (let i = 0; i < data.length; i++)
+        {
+            let work = data[i];
+            let title = work.title;
+            let url = work.imageUrl;
+
+            console.log(title);
+            url = url.replace("http://localhost:5678", "./assets");
+            console.log(url);
+
+            let figure = document.createElement("figure");
+            let img = document.createElement("img");
+            let figcaption = document.createElement("figcaption");
+            let gallery = document.getElementsByClassName("gallery")[0];
+            img.setAttribute("src", work.imageUrl);
+            img.setAttribute("crossorigin","anonymous")
+            figcaption.append(title);
+            figure.appendChild(img);
+            figure.appendChild(figcaption);
+            gallery.appendChild(figure);
+        }})
+
+fetch("http://localhost:5678/api/categories")
+.then((reponse) => reponse.json())
+    .then (donnee => console.log(donnee));
