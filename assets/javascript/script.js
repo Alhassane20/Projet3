8641a5daabbb90fb705af6f763f6c649b8351915
Alhassane Seye <seyealhassane@gmail.com>
2023-02-06 21:31:40 +0100

    ajouts

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,53 +1,84 @@
+
 fetch("http://localhost:5678/api/works")
     .then((response) => response.json())
-    .then (data =>{console.log(data);
-        for (var i = 0; i < data.length; i++)
-        {
-            var work = data[i];
-            var title = work.title;
-            var url = work.imageUrl;
+    .then(data => {
+        console.log(data);
+        fetchCategories(data)
+    })
 
-            console.log(title);
-            url = url.replace("http://localhost:5678", "./assets");
-            console.log(url);
 
-            var figure = document.createElement("figure");
-            var img = document.createElement("img");
-            var figcaption = document.createElement("figcaption");
-            var gallery = document.getElementsByClassName("gallery")[0];
-            img.setAttribute("src", work.imageUrl);
-            img.setAttribute("crossorigin","anonymous")
-            figcaption.append(title);
-            figure.appendChild(img);
-            figure.appendChild(figcaption);
-            gallery.appendChild(figure);
+async function fetchCategories(works) {
+ fetch("http://localhost:5678/api/categories")
+        .then((reponse) => reponse.json())
+        .then(categories => {
+            console.log(categories);
+            var filtres = document.getElementById("filtres");
+            categories.forEach(cat => {
+                filterFactory(cat);
+                works.forEach(b =>{
+                    workFactory(b);
+                });
+            });
 
-            var categorie = work.categoryId
-            console.log(categorie);
-        }})
+        });
+        
+}
 
-fetch("http://localhost:5678/api/categories")
-.then((reponse) => reponse.json())
-.then (categories => {console.log(categories)
-    var filtres = document.getElementById("filtres")
-    for (var i = 0; i < categories.length; i++){
-        var category = categories[i]
-        var div = document.createElement("div")
-        div.setAttribute("class","filtre")
-        var p = document.createElement("p")
-        p.append(category.name)
-        div.appendChild(p)
-        filtres.appendChild(div)
-        console.log(div);
-        var id = category.id
-        console.log(id);
-        const categorie1 = new Set
-        categorie1.add(id)
-        const categorie2 = new Set
-        categorie2.add(id)
-        const categorie3 = new Set
-        categorie3.add(id)
+function filterByCategory(categoryId) {
+    let figures = document.getElementsByClassName("figure-work");
+    for(var i = 0; i < figures.length; ++i) {
+        let figure = figures[i];
+        let categoryWork =  parseInt(figure.getAttribute("categoryId"));
+        
+        if(categoryId == categoryWork || categoryId == -1) {
+            figure.style.display = "block";
+        } else {
+            figure.style.display = "none";
+        }
+    }
+}
 
-}})
+function filterFactory(category) {
+    div = document.createElement("div")
+    div.setAttribute("class", "filtre")
+    var p = document.createElement("p")
+    p.append(category.name)
+    div.appendChild(p)
+    div.setAttribute("onclick",`filterByCategory(${category.id})`)
+    filtres.appendChild(div)
+}
 
-    
+function workFactory(work) {
+    var title = work.title;
+    var url = work.imageUrl;
+
+    var figure = document.createElement("figure");
+    var img = document.createElement("img");
+    var figcaption = document.createElement("figcaption");
+    var gallery = document.getElementsByClassName("gallery")[0];
+    img.setAttribute("src", work.imageUrl);
+    img.setAttribute("crossorigin", "anonymous")
+    figcaption.append(title);
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+    gallery.appendChild(figure);
+    figure.setAttribute("categoryId", work.categoryId);
+    figure.setAttribute("class", "figure-work");
+
+}
+
+async function Connection() {
+    let user = document.getElementById("user").value;
+    let password = document.getElementById("password").value;
+
+    fetch("http://localhost:5678/api/users/login", {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+        "email": "string",
+        "password": "string",
+    })
+    }).then((response) => response.json())
+    .then(data =>
+        console.log(data));
+}
