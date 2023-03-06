016794bba27091c017e2456d500bf1ce57b919d2
Alhassane Seye <seyealhassane@gmail.com>
2023-03-06 21:42:34 +0100

    ajouts

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,35 +2,34 @@
 fetch("http://localhost:5678/api/works")
     .then((response) => response.json())
     .then(data => {
-        console.log(data);
         fetchCategories(data)
     })
 
 
 async function fetchCategories(works) {
- fetch("http://localhost:5678/api/categories")
+    fetch("http://localhost:5678/api/categories")
         .then((reponse) => reponse.json())
         .then(categories => {
-            console.log(categories);
             var filtres = document.getElementById("filtres");
             categories.forEach(cat => {
                 filterFactory(cat);
-                works.forEach(b =>{
+                works.forEach(b => {
                     workFactory(b);
                 });
             });
 
         });
-        
+
 }
 
 function filterByCategory(categoryId) {
+    // filtrer les les travaux par categories
     let figures = document.getElementsByClassName("figure-work");
-    for(var i = 0; i < figures.length; ++i) {
+    for (var i = 0; i < figures.length; ++i) {
         let figure = figures[i];
-        let categoryWork =  parseInt(figure.getAttribute("categoryId"));
-        
-        if(categoryId == categoryWork || categoryId == -1) {
+        let categoryWork = parseInt(figure.getAttribute("categoryId"));
+
+        if (categoryId == categoryWork || categoryId == -1) {
             figure.style.display = "block";
         } else {
             figure.style.display = "none";
@@ -39,16 +38,18 @@ function filterByCategory(categoryId) {
 }
 
 function filterFactory(category) {
+    // creer les filtres dynamiquement
     div = document.createElement("div")
     div.setAttribute("class", "filtre")
     var p = document.createElement("p")
     p.append(category.name)
     div.appendChild(p)
-    div.setAttribute("onclick",`filterByCategory(${category.id})`)
+    div.setAttribute("onclick", `filterByCategory(${category.id})`)
     filtres.appendChild(div)
 }
 
 function workFactory(work) {
+    // créer les travaux dynamiquement
     var title = work.title;
     var url = work.imageUrl;
 
@@ -68,46 +69,202 @@ function workFactory(work) {
 }
 
 async function Connection() {
+    // authentification
     let user = document.getElementById("user").value;
     let password = document.getElementById("password").value;
 
     fetch("http://localhost:5678/api/users/login", {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({
-        "email": user,
-        "password": password,
-    })
-    // Remplir le formulaire
-    // Envoyer le formulaire
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            "email": user,
+            "password": password,
+        })
+        // Remplir le formulaire
+        // Envoyer le formulaire
     }).then((response) => response.json())
-    .then(data => {
-        if (data.token == null) {
-            let message = data.message
-            document.body.append(message)
-            // Si connexion échouée
-            // Recois aucun token 
-            // Recois un message d'erreur
-        } else {
-            // Si connexion reussie
-            // Recevoir le token
-            // Stocker le token
-            // Rediriger vers la page d'accueil
-        }
-        localStorage.setItem("token","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4")
-    })
+        .then(data => {
+            if (data.token == null) {
+                let message = data.message
+                document.body.append(message)
+                // Si connexion échouée
+                // Recois aucun token 
+                // Recois un message d'erreur
+            } else {
+                // localStorage.setItem("token", data.token)
+                window.location = "index.html?token=" + data.token;
+                // Si connexion reussie
+                // Recevoir le token
+                // Stocker le token
+                // Rediriger vers la page d'accueil
+            }
+
+        })
 }
 
-let rectangle = document.getElementById("rectangle")
-rectangle.style.display = "none"
-let modifierphoto = document.getElementById("modifierphoto")
-modifierphoto.style.display = "none"
-let modifierworks = document.getElementById("modifierworks")
-modifierworks.style.display = "none"
-function Redirection() {
-    window.location.replace=("index.html");
-    rectangle.style.display = "block"
-    modifierphoto.style.display = "block"
-    modifierworks.style.display = "block"
 
+
+function getToken() {
+    const urlParams = new URLSearchParams(window.location.search);
+    let token = urlParams.get('token')
+    return token;
+}
+
+if (getToken() != null) {
+    // Modifications pour page connecté
+    let rectangle = document.getElementById("rectangle")
+    rectangle.style.display = "flex"
+    let boutonmodal = document.getElementById("boutonmodal")
+    boutonmodal.style.display = "flex"
+    let modifierworks = document.getElementById("modifierworks")
+    modifierworks.style.display = "flex"
+    // Ajouter "token" dans une variable
+    //  Si localStorage contient token
+    //  Affiche les boutons
+}
+
+function fermerModale() {
+    let modaleContener = document.querySelector(".modalContener")
+    modaleContener.style.display = "none";
+}
+
+function ouvrirModale() {
+    let modaleContener = document.querySelector(".modalContener")
+    modaleContener.style.display = "block"
+
+    worksModale();
+}
+
+document.addEventListener("click", (e) => {
+    if (e.target.id === "modaleContener") {
+        fermerModale()
+    }
+})
+
+function worksModale() {
+    fetch("http://localhost:5678/api/works")
+        .then((response) => response.json())
+        .then(data => {
+            for (let i = 0; i < data.length; ++i) {
+                workModalFactory(data[i]);
+            }
+        })
+}
+
+function workModalFactory(work) {
+    let images = document.getElementById("images")
+
+    let imge = document.createElement("div")
+    imge.setAttribute("class", "imge")
+
+    let imgwork = document.createElement("img")
+    imgwork.setAttribute("src", work.imageUrl)
+    imgwork.setAttribute("crossorigin", "anonymous")
+
+    let croix = document.createElement("i")
+    croix.setAttribute("class", "fa-solid fa-arrows-up-down-left-right arrow")
+
+    let poubelle = document.createElement("i")
+    poubelle.setAttribute("class", "fa-sharp fa-solid fa-trash-can poubelle")
+    poubelle.setAttribute("click", deleteOneWork(id))
+
+    let editer = document.createElement("figcaption")
+    editer.append("éditer")
+
+    imge.appendChild(imgwork)
+    imge.appendChild(croix)
+    imge.appendChild(poubelle)
+    imge.appendChild(editer)
+    images.appendChild(imge)
+}
+
+function deleteWorks() {
+    // fetch pour récup les works avec fetch
+    // pour chaque works de ta liste
+    // 
+    fetch("http://localhost:5678/api/works")
+        .then((response) => response.json())
+        .then(data => {
+            for (i = 0; i < data.length; i++) {
+                let id = data[i].id
+                deleteOneWork(id)
+            }
+        })
+
+    window.location.reload()
+}
+
+function deleteOneWork(id) {
+    let url = "http://localhost:5678/api/works/" + id;
+    fetch(url, {
+        method: 'DELETE',
+        headers: {
+            "Authorization": `Bearer ${getToken()}`
+        }
+    }).then((response) => response.json())
+        .then((data) => {
+        })
+}
+
+function changerModale() {
+    let modale1 = document.getElementById("modale1")
+    let modale2 = document.getElementById("modale2")
+    modale1.style.display = "none"
+    modale2.style.display = "block"
+    categoriesModale()
+}
+function returnModal() {
+    let modale1 = document.getElementById("modale1")
+    let modale2 = document.getElementById("modale2")
+    modale1.style.display = "block"
+    modale2.style.display = "none"
+}
+
+function addWorks(e) {
+    let image = document.getElementById("addPhotos")
+    let titre = document.getElementById("titre").value;
+    let categorie = document.getElementById("categorie").value;
+    let form = document.getElementById("modale2")
+    fetch("http://localhost:5678/api/works", {
+        method: 'POST',
+        headers: {
+            'accept': 'application/json',
+            'Content-Type': 'multipart/form-data',
+            "Authorization": `Bearer ${getToken()}`,
+        },
+        body: new FormData(document.getElementById('modale2'))
+        // body: JSON.stringify({
+        //     "image": image,
+        //     "title": titre,
+        //     "category": categorie,
+        // })
+    }).then((response) => response.json())
+        .then(data => {
+            console.log(data);
+            let erreur = data.error
+            if (image =! "file") {
+        document.body.append(erreur)
+    }
+            if (titre =! "string") {
+        document.body.append(erreur)
+    }
+            if (categorie =! "string") {
+        document.body.append(erreur)
+    }
+})}
+
+function categoriesModale() {
+    fetch("http://localhost:5678/api/categories")
+    .then((response) => response.json())
+        .then(data => {
+            let categorie = document.getElementById("categorie")
+            categorie.innerHTML = '';
+            for (let i = 0; i < data.length; i++) {
+                let option = document.createElement("option");
+                option.setAttribute("value", data[i].id)
+                option.append(data[i].name);
+                categorie.appendChild(option)
+            }
+
+        })
 }
